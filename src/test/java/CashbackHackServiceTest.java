c2fb39da5970d1e297bfeefd0c0a02bc03585c85
lavie head <nadya.karpenko@gmail.com>
2023-12-06 16:39:34 +0000

    junit4 commit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import org.junit.Assert;
+import ru.netology.CashbackHackService;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void ShouldCalculateCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 500;
+        int actual = service.remain(1500);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldCalculateCashbackZero() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldCalculateCashbackOne() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldCalculateCashbackThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
